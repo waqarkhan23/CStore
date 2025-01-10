14dee3d44229023b7fc908f8b49d0630b9e862e2
waqar-crewlogix <waqar.c@crewlogix.com>
2025-01-10 15:17:09 +0500

    new

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button";
+
+function App() {
+  return (
+    <>
+      <h1>Hello World!</h1>
+      <Button>click me</Button>
+    </>
+  );
+}
+
+export default App;
